14975601192cb8c9459482eba6669e19606a366e
Melvillian <melvillevt@gmail.com>
2023-05-29 22:00:19 -0400

    complete tutorial

---
+++ README.md
@@ -0,0 +1,10 @@
+# Deno Langchain Example
+
+Built using [the cloudflare tutorial](https://blog.cloudflare.com/langchain-and-cloudflare/), but with [Deno](https://deno.com/runtime).
+
+## Usage
+
+```bash
+# Note: must install deno first
+deno task dev
+```
--- main.ts
+++ main.ts
@@ -1,7 +1,40 @@
-import { serve } from "https://deno.land/std@0.189.0/http/server.ts";
 import { load } from "https://deno.land/std/dotenv/mod.ts";
+import { serve } from "https://deno.land/std@0.189.0/http/server.ts";
+
+import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { OpenAI } from "langchain/llms/openai";
+import { RetrievalQAChain } from "langchain/chains";
 
 const env = await load();
-const key = env["OPENAI_KEY"];
 
-console.log(key);
+const handler = async (request: Request): Promise<Response> => {
+  
+  const loader = new CheerioWebBaseLoader(
+    "https://en.wikipedia.org/wiki/Brooklyn"
+  );
+  // load wiki page and split into smaller documents
+  const docs = await loader.loadAndSplit();
+
+  // setup an in-memory vector store using OpenAI embeddings
+  const store = await MemoryVectorStore.fromDocuments(docs, new OpenAIEmbeddings({ openAIApiKey: env["OPENAI_API_KEY"] }));
+
+  // setup a retrieval-based QA chain
+  const model = new OpenAI({ openAIApiKey: env["OPENAI_API_KEY"]});
+  const chain = RetrievalQAChain.fromLLM(model, store.asRetriever());
+
+  // our prompt
+  // const question = "What is this article about? Can you give me 3 facts about it?";
+  const { searchParams } = new URL(request.url);
+  const question = searchParams.get('question') ?? "What is this article about? Can you give me 3 facts about it?";
+
+  const res = await chain.call({
+      query: question,
+  });
+
+  console.log(res.text);
+  return new Response(res.text);
+};
+
+serve(handler);
