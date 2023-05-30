3e1768eac3d96638b6557b9908361ce3c8deda20
Melvillian <melvillevt@gmail.com>
2023-05-29 21:00:39 -0400

    add env var handling

--- main.ts
+++ main.ts
@@ -1,22 +1,7 @@
 import { serve } from "https://deno.land/std@0.189.0/http/server.ts";
+import { load } from "https://deno.land/std/dotenv/mod.ts";
 
-const handler = async (_request: Request): Promise<Response> => {
-  const resp = await fetch("https://api.github.com/users/denoland", {
-    // The init object here has an headers object containing a
-    // header that indicates what type of response we accept.
-    // We're not specifying the method field since by default
-    // fetch makes a GET request.
-    headers: {
-      accept: "application/json",
-    },
-  });
+const env = await load();
+const key = env["OPENAI_KEY"];
 
-  return new Response(resp.body, {
-    status: resp.status,
-    headers: {
-      "content-type": "application/json",
-    },
-  });
-};
-
-serve(handler);
+console.log(key);
